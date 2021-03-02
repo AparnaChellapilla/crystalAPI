d483cdc8ac309676a74ba070f592540a1fc467f8
AC <75492066+AparnaChellapilla@users.noreply.github.com>
2021-03-02 11:06:54 -0600

    maven validation
    Added  maven validation in build.gradle

--- src/main/java/com/tts/crystalAPI/model/Crystal.java
+++ src/main/java/com/tts/crystalAPI/model/Crystal.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Crystal {
@@ -13,6 +14,8 @@ public class Crystal {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String color;
+
+    @Size(max = 15)
     private String name;
 
     public Long getId() {
